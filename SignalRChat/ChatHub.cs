b9cdebe184fffd63fab9917a12725c2945bf81b1
Yuri Kosenko <krakoss@mail.ru>
2023-12-01 06:30:49 +0300

    new

--- SignalRChat/ChatHub.cs
+++ SignalRChat/ChatHub.cs
@@ -1,9 +1,7 @@
-﻿using System;
+﻿using Microsoft.AspNet.SignalR;
+using SignalRChat.Common;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
-using Microsoft.AspNet.SignalR;
-using SignalRChat.Common;
 
 namespace SignalRChat
 {
